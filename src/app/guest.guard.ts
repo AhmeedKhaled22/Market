0de6dadc3534a8d5c44ec779e5dd532bad66fa13
Ahmed Khaled <wascsacaca@gmail.com>
2026-04-14 21:11:12 +0200

    fix guards

--- src/app/guest.guard.ts
+++ src/app/guest.guard.ts
@@ -2,18 +2,14 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { Auth } from '@angular/fire/auth';
 
-export const guestGuard: CanActivateFn = async () => {
+export const guestGuard: CanActivateFn = () => {
 
   const auth = inject(Auth);
   const router = inject(Router);
 
-  await auth.authStateReady();
-
-  // لو المستخدم مش مسجل دخول
   if (!auth.currentUser) {
     return true;
   }
 
-  // لو مسجل دخول يرجعه home
   return router.createUrlTree(['/home']);
 };
